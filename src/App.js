cbda7884fb6fe292ec34c0be90feaac87c6f17a3
roughjs247 <roughjs247@naver.com>
2020-09-26 23:33:23 +0900

    customer 컴포넌트 추가

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Customer from './components/Customer';
+
+const customers = [
+    {
+    'id'   : 1,
+    'image': 'https://placeimg.com/64/64/any',
+    'name' : '이용빈',
+    'birthday' : '900918',
+    'gender' : 'male',
+    'job'    : 'developer',
+    },
+    {
+      'id'   : 2,
+      'image': 'https://placeimg.com/64/64/1',
+      'name' : '홍길동',
+      'birthday' : '131244',
+      'gender' : 'female',
+      'job'    : 'student',
+      },
+      {
+        'id'   : 3,
+        'image': 'https://placeimg.com/64/64/2',
+        'name' : '김길수',
+        'birthday' : '515122',
+        'gender' : 'male',
+        'job'    : 'photograper',
+        },          
+];
+
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <div>
+      {
+        customers.map(c => {
+          return(
+              <Customer  key={c.id}  id={c.id} image={c.image} name={c.name} birthday={c.birthday} gender={c.gender} job={c.job} />
+             )
+          }
+        )
+      }
+    </div>        
   );
 }
 
