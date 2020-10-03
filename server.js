6becd1aa4224964c5c04e8ede8bfa6d842760e63
이용빈 <iyongbin@iyongbin-ui-MacBookPro.local>
2020-10-04 02:49:52 +0900

    mysql connection

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
@@ -6,34 +7,24 @@ const port = 5000 || process.env.port;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
 
+// database connection
+const data = fs.readFileSync('./database.json');
+const  conf = JSON.parse(data);
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+    host : conf.host,
+    user : conf.user,
+    password : conf.password,
+    port : conf.port,
+    database : conf.database
+});
+connection.connect();
 //get
 app.get('/api/customers', (req,res) => {
-    res.send([
-        {
-        'id'   : 1,
-        'image': 'https://placeimg.com/64/64/any',
-        'name' : '이용빈',
-        'birthday' : '900918',
-        'gender' : 'male',
-        'job'    : 'developer',
-        },
-        {
-        'id'   : 2,
-        'image': 'https://placeimg.com/64/64/1',
-        'name' : '홍길동',
-        'birthday' : '131244',
-        'gender' : 'female',
-        'job'    : 'student',
-        },
-        {
-        'id'   : 3,
-        'image': 'https://placeimg.com/64/64/2',
-        'name' : '김길수',
-        'birthday' : '515122',
-        'gender' : 'male',
-        'job'    : 'photograper',
-        },          
-    ]);
+    connection.query("SELECT * FROM CUSTOMER", (err, rows, fields) => {
+        res.send(rows);
+    });
 });
 
 app.listen(port, () => console.log(`Node server start! ${port}`));
