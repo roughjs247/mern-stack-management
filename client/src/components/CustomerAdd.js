daf15755cffebe42641583bf5b39e2f62c6486a1
이용빈 <iyongbin@iyongbin-ui-MacBookPro.local>
2020-10-04 18:11:29 +0900

    delete 기능 추가

--- client/src/components/CustomerAdd.js
+++ client/src/components/CustomerAdd.js
@@ -1,5 +1,18 @@
 import React from 'react';
 import { post } from 'axios';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+import {withStyles} from '@material-ui/core/styles';
+
+const styles = theme => ({
+    hidden : {
+        display : 'none'
+    }     
+});
 
 class CustomerAdd extends React.Component {
 
@@ -11,7 +24,8 @@ class CustomerAdd extends React.Component {
             birthday : '',
             gender : '',
             job : '',
-            fileName : ''
+            fileName : '',
+            open : false // dialog State
         }
     }
 
@@ -27,7 +41,8 @@ class CustomerAdd extends React.Component {
             birthday : '',
             gender : '',
             job : '',
-            fileName :''
+            fileName :'',
+            open : false
         })   
     };
 
@@ -56,20 +71,55 @@ class CustomerAdd extends React.Component {
         return await post(url, formData, config); // 서버로 Post 방식으로 Data를 보낸다.
     }
 
+    // modal Flag
+    handleClickOpen= () => {
+        this.setState({
+            open : true
+        })
+    }
+
+    handleClose = () => {
+        this.setState({
+            file : null,
+            usernamne : '',
+            birthday : '',
+            gender : '',
+            job : '',
+            fileName :'',
+            open : false
+        })   
+    }
+
     render() {
+        const { classes } = this.props;
+
         return (
-            <form onSubmit={this.handleFormSubmit}>
-                <h1>고객 추가</h1>
-                프로필 이미지 : <input type="file" name="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
-                이름 : <input type="text" name="username" value={this.state.username} onChange={this.handleValueChange}/><br/>
-                생년월일 : <input type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
-                성별 : <input type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
-                직업 : <input type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>
-                <button type="submit">추가</button>
-            </form>
+            <div>
+                <Button variant="contained" color="primary" onClick={this.handleClickOpen}> 고객 추가하기 </Button>
+                <Dialog open={this.state.open} onClose={this.state.handleClose}>
+                    <DialogTitle>고객 추가</DialogTitle>
+                    <DialogContent>
+                        <input className={classes.hidden} accept="image/*" id="rasied-button-file" type="file" file={this.state.file} value={this.state.fileName} onChange={this.handleFileChange}/><br/>
+                        <label htmlFor="rasied-button-file" >
+                            <Button variant="contained" color="primary" component="span" name="file">
+                                {this.state.fileName === "" ? "프로필 이미지 선택" : this.state.fileName}
+                            </Button>
+                        </label>
+                        <br/>
+                        <TextField label ="이름" type="text" name="username" value={this.state.username} onChange={this.handleValueChange}/><br/>
+                        <TextField label ="생년월일" type="text" name="birthday" value={this.state.birthday} onChange={this.handleValueChange}/><br/>
+                        <TextField label ="성별" type="text" name="gender" value={this.state.gender} onChange={this.handleValueChange}/><br/>
+                        <TextField label ="직업" type="text" name="job" value={this.state.job} onChange={this.handleValueChange}/><br/>                        
+                    </DialogContent>
+                    <DialogActions>
+                        <Button variant="contained" color="primary" onClick={this.handleFormSubmit}>추가</Button>
+                        <Button variant="outlined" color="primary" onClick={this.handleClose}>닫기</Button>
+                    </DialogActions>
+                </Dialog>
+            </div>
         )
     }
 
 }
 
-export default CustomerAdd;
+export default withStyles(styles)(CustomerAdd); // withStyles이 적용되어 모듈을 내보낼때 감싸줘야함.
